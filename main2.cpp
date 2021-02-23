486e87f52c261ff41902d3459c75a5ce05792a30
Lucas Vieira <lucas208@ufrn.edu.br>
2021-02-22 21:39:38 -0300

    Atualizando pra f2

--- main2.cpp
+++ main2.cpp
@@ -1,34 +1,47 @@
 #include <iostream>
 #include <pthread.h>
 #include <unistd.h>
+#include <math.h>
 
 using namespace std;
 
+#define PI 3.14159265358979323846  
+
 double a = 0;
-double b = 10;
-double n = 120;
-int t = 6;
-double soma[6];
+double b = 2*PI;
+int n = 200;
+int t = 7;
+double soma[7];
 
 double f(double x){
     return 5;
 }
 
+double f2(double x){
+    return sin(2*x)+cos(5*x);
+}
+
 void* computar(void *tid){
 
     int rank = (int)(size_t)tid;
-    double local_n = n/t;
+
+    int local_n = n/t;
+
+    if(rank == t-1){
+        local_n += n%t;
+    }    
+
     double largura_trapezio = (b-a)/n;
     cout<<"Esta é a Thread "<< rank << endl;
     double local_a = a + rank*largura_trapezio*local_n;
     double local_b = local_a + largura_trapezio*local_n;
     double x_i;
     double h = (local_b - local_a)/local_n;
-    double area_total = (f(local_a)+f(local_b))/2;
-    
+    double area_total = (f2(local_a)+f2(local_b))/2;
+
     for(int i = 1;i<local_n;i++){
         x_i = local_a +i*h;
-        area_total += f(x_i);
+        area_total += f2(x_i);
     }
 
     area_total = h*area_total;
